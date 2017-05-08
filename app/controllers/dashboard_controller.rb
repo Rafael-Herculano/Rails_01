75687469bd585cd9276cba7fbcbded1a77f15325
Sidney Oliveira <sidney-santana@live.com>
2017-05-08 00:51:53 -0300

    desenvolvimento do layout padrão

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,8 @@
+class DashboardController < ApplicationController
+  def index
+    flash[:success ] = "Bem vindo!"
+    #other alternatives are
+    # flash[:warn ] = "Israel don't quite like warnings"
+    #flash[:danger ] = "Naomi let the dog out!"
+  end
+end
