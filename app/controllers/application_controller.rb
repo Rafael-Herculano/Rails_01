2aba70881bf3c92f04dad4507f3bde7cbdcc2e33
Sidney Oliveira <sidney.santana@abril.com.br>
2017-05-16 15:10:22 -0300

    resolução do menu serviços

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :get_servicos
+
+  def get_servicos
+  	teste = servicos_path
+  end
 end
